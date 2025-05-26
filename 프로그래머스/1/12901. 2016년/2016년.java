83bc036fdb372ed609c1455922be7f027bd79151
박성현 <pjhok2002@naver.com>
2025-05-26 09:58:48 +0900

    [level 1] Title: 2016년, Time: 0.04 ms, Memory: 88.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/12901. 2016년/2016년.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String solution(int a, int b) {
+        String[] answer = {"FRI", "SAT", "SUN","MON","TUE","WED","THU"};
+        
+        int[] monthDays = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30 ,31};
+        
+        int total = 0;
+        
+        for (int i = 0; i<a-1; i++) {
+            total += monthDays[i];
+        }
+        total += b -1;
+        
+        return answer[total % 7];
+    }
+}
